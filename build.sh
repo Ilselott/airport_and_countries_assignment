ac0689dfb8e04cae623098252f6202c6e8c2ed2b
Loelia <loelia.thevenin@gmail.com>
2019-06-19 11:26:58 +0200

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build --build-arg SERV=airports --build-arg=VERS=1.0.1 -t airports:1.0.1 .
-docker build --build-arg SERV=airports --build-arg=VERS=1.1.0 -t airports:1.1.0 .
-docker build --build-arg SERV=countries --build-arg=VERS=1.0.1 -t countries:1.0.1 .
-docker build -t loadbalancer:1.0.0 -f loadbalancerDockerfile .
+sudo docker build --build-arg SERV=airports --build-arg=VERS=1.0.1 -t ilselott/airports:1.0.1 .
+sudo docker build --build-arg SERV=airports --build-arg=VERS=1.1.0 -t ilselott/airports:1.1.0 .
+sudo docker build --build-arg SERV=countries --build-arg=VERS=1.0.1 -t ilselott/countries:1.0.1 .
+sudo docker build -f LBDockerfile -t ilselott/loadbalancer:1.0.0 .
