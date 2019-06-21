8b49f339aa478bdaaea0e4dc5571aa9c9a166660
Loelia <loelia.thevenin@gmail.com>
2019-06-21 15:00:30 +0200

    installation script

---
+++ terraform/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install docker -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod 755 /usr/local/bin/docker-compose
+sudo service docker start
+
+sudo curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/terraform/curl_files.sh > /home/ec2-user/curl_files.sh
+sudo chmod 755 /home/ec2-user/curl_files.sh
+sudo ./home/ec2-user/curl_files.sh
+sudo docker swarm init
+cd /home/ec2-user/fly/docker-swarm
+docker-compose up
