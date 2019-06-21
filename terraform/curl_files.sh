f67a108c0dfcc36684d1bc57b93218b727180e21
Loelia <loelia.thevenin@gmail.com>
2019-06-21 16:31:09 +0200

    changed all directories to end up in final directory

--- terraform/curl_files.sh
+++ terraform/curl_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
-mkdir /home/ec2-user/sums /home/ec2-user/nginx /home/ec2-user/docker-swarm /home/ec2-user/nginx/configs fly
+mkdir /home/ec2-user/fly/sums /home/ec2-user/fly/nginx /home/ec2-user/fly/docker-swarm /home/ec2-user/fly/nginx/configs fly
 touch nginx/configs/error.log
-echo "1" > /home/ec2-user/nginx/configs/nginx.pid
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/docker-swarm/docker-compose.yml > /home/ec2-user/docker-swarm/docker-compose.yml 
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/sums/airports-1.0.1.txt
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/sums/airports-1.1.0.txt
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/sums/countries-1.0.1.txt
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/Dockerfile > /home/ec2-user/Dockerfile
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/LBDockerfile > /home/ec2-user/LBDockerfile
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/build.sh > /home/ec2-user/build.sh
-curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/nginx/configs/nginx.conf > /home/ec2-user/nginx/configs/nginx.conf
+echo "1" > /home/ec2-user/fly/nginx/configs/nginx.pid
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/docker-swarm/docker-compose.yml > /home/ec2-user/fly/docker-swarm/docker-compose.yml 
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/airports-1.0.1.txt
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/airports-1.1.0.txt
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/countries-1.0.1.txt
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/Dockerfile > /home/ec2-user/fly/Dockerfile
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/LBDockerfile > /home/ec2-user/fly/LBDockerfile
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/build.sh > /home/ec2-user/fly/build.sh
+curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/nginx/configs/nginx.conf > /home/ec2-user/fly/nginx/configs/nginx.conf
