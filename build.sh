1ab99e9c2c896cc971b52870ff1673f90020a4d8
Loelia <loelia.thevenin@gmail.com>
2019-06-21 11:09:15 +0200

    added a shebang to the script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo docker build --build-arg SERV=airports --build-arg=VERS=1.0.1 -t ilselott/airports:1.0.1 .
 sudo docker build --build-arg SERV=airports --build-arg=VERS=1.1.0 -t ilselott/airports:1.1.0 .
 sudo docker build --build-arg SERV=countries --build-arg=VERS=1.0.1 -t ilselott/countries:1.0.1 .
