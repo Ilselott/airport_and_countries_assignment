22208138a24b99907c9789de617a2442d3780e87
Loelia <loelia.thevenin@gmail.com>
2019-06-26 14:14:21 +0200

    modified the deployment script for kubernetes

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,5 +6,7 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/mast
 ## Create the applications pods and services
 kubectl apply -f $(dirname $0)/airports.yaml
 kubectl apply -f $(dirname $0)/countries.yaml
+#destroy any previous load balancer
+kubectl delete Ingress exemple-ingress
 # Launches the Ingress load balancer and reverse proxy
 kubectl create -f $(dirname $0)/ingress.yaml
