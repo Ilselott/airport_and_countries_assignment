5c6218fbae293e1bbb23e1bcd06e19e87de51ab9
Loelia <loelia.thevenin@gmail.com>
2019-06-26 10:28:10 +0200

    changed path to curl for docker-compose.yml

--- terraform/curl_files.sh
+++ terraform/curl_files.sh
@@ -4,7 +4,7 @@ sudo mkdir /home/ec2-user/fly/
 sudo mkdir /home/ec2-user/fly/sums /home/ec2-user/fly/nginx /home/ec2-user/fly/docker-swarm /home/ec2-user/fly/nginx/configs
 touch nginx/configs/error.log
 echo "1" > /home/ec2-user/fly/nginx/configs/nginx.pid
-curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/docker-swarm/docker-compose.yml > /home/ec2-user/fly/docker-swarm/docker-compose.yml 
+curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/docker-swarm/docker-compose.yml > /home/ec2-user/fly/docker-swarm/docker-compose.yml 
 curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/airports-1.0.1.txt
 curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/airports-1.1.0.txt
 curl https://raw.githubuser/flycontent.com/Ilselott/airport_and_countries_assignment/master/sums/airports-1.0.1.txt > /home/ec2-user/fly/sums/countries-1.0.1.txt
