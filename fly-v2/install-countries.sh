bad290c2e0926f33d1401cbdfca71124da866309
Loelia <loelia.thevenin@gmail.com>
2019-06-28 15:24:54 +0200

    typo correction in script

--- fly-v2/install-countries.sh
+++ fly-v2/install-countries.sh
@@ -7,5 +7,5 @@ sudo service docker start
 sudo curl https://raw.githubusercontent.com/Ilselott/airport_and_countries_assignment/master/fly-v2/curl_files.sh > /home/ec2-user/curl_files.sh
 sudo chmod 755 /home/ec2-user/curl_files.sh
 sudo ./home/ec2-user/curl_files.sh
-sudo docker build --build-arg SERV=countries --build-arg=VERS=1.0.1 -t airports:1.0.1 /home/ec2-user/fly
-sudo docker run airports:1.0.1
+sudo docker build --build-arg SERV=countries --build-arg=VERS=1.0.1 -t countries:1.0.1 /home/ec2-user/fly
+sudo docker run countries:1.0.1
